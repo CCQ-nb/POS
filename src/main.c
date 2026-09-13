fde2c09f70768313669126b59ccba149e61c3d8c
Q3U2 <U202613588@hust.edu.cn>
2026-09-13 23:23:44 +0800

    feat:读取用户输入，打开items.csv文件并读取信息，构建学习文档

--- src/main.c
+++ src/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+#define MAX_LINE 256
 
-int main(void) {
-    printf("Hello, POS!\n");
+/*定义结构体*/
+typedef struct{
+    char code[16];
+    char name[32];
+    int price;
+    int stock;
+} Item;
+
+/*初始化商品信息(待分开写)*/
+
+
+int main(){
+
+    /*读取用户输入*/
+    char line[MAX_LINE];
+    fgets(line, sizeof(line), stdin);
+    line[strcspn(line, "\r\n")] = '\0';
+
+    printf("%s\n", line);
+
     return 0;
 }
+
